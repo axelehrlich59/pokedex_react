1146edb91fa6314d66d1375f1e7877147fd5313b
Axel <axelehrlich8@gmail.com>
2021-12-17 09:26:53 +0100

    api pokemon request in json

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
   const [allPokemons, setAllPokemon] = useState([])
   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
 
+  const getAllPokemons = async () => {
+    const resultPokemons = await fetch(loadMore)
+    const dataRequest = resultPokemons.json()
+    console.log(dataRequest)
+  }
+
 
   return (
-    <div className="App">
+    <div className="app-container">
       <div className='main_container'>
-        <h1>Hello</h1>
-      </div>
-      
+          <h1>Pokemon Evolution</h1>
+        <div className="pokemon-container">
+
+        </div>
+        <button className='load-more'>Load more</button>
+      </div> 
     </div>
   );
 }
